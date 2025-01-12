5ec6beff5da3f36713ebc298f344740241e164b6
naobum <artur130605@gmail.com>
2025-01-12 20:26:33 +0300

    first commit

---
+++ Life Game Console Edition/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life_Game_Console_Edition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+
+            var gameEngine = new GameLogic
+                (
+                cols: 358,
+                rows: 100,
+                density: 2
+                );
+
+            while (true)
+            {
+                Console.Title = gameEngine.CurrentGeneration.ToString();
+
+                var field = gameEngine.GetCurrentGeneration();
+
+                for (int y = 0; y < field.GetLength(1); y++)
+                {
+                    var str = new char[field.GetLength(0)];
+
+                    for (int x = 0; x < field.GetLength(0); x++)
+                    {
+                        if (field[x, y])
+                            str[x] = '#';
+                        else
+                            str[x] = ' ';
+                    }
+                    Console.WriteLine(str);
+                }
+                Console.SetCursorPosition(0, 0);
+                gameEngine.UpdateGeneration();
+                
+            }
+        }
+    }
+}
---
+++ Life Game/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Life_Game
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private int resolution;
+        private GameEngine engine;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void StartGame()
+        {
+            if (timer1.Enabled) 
+                return;
+            
+            nudResolution.Enabled = false;
+            nudDensity.Enabled = false;
+            resolution = (int)nudResolution.Value;
+
+            engine = new GameEngine
+                (
+                    rows: pictureBox1.Height / resolution,
+                    cols: pictureBox1.Width / resolution,
+                    density: (int)nudDensity.Minimum + (int)nudDensity.Maximum - (int)nudDensity.Value
+                );
+
+            Text = $"Generation {engine.CurrentGeneration}";
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
+        }
+        private void DrawNextGeneration()
+        {         
+            graphics.Clear(Color.DarkBlue);
+
+            var field = engine.GetCurrentGeneration();
+
+            for (int x = 0; x < field.GetLength(0); x++)
+            {
+                for (int y = 0; y < field.GetLength(1); y++)
+                {
+                    if (field[x, y])
+                        graphics.FillRectangle(Brushes.LightGreen, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                }
+            }
+            pictureBox1.Refresh();
+            Text = $"Generation {engine.CurrentGeneration}";
+            engine.NextGeneration();
+        }
+        
+
+        private void StopGame()
+        {
+            if (!timer1.Enabled)
+                return;
+            timer1.Stop();
+            nudResolution.Enabled = true;
+            nudDensity.Enabled = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DrawNextGeneration();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                engine.AddCell(x, y);
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;   
+                engine.RemoveCell(x, y);
+            }
+        }
+
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            StopGame();
+        }
+    }
+}
